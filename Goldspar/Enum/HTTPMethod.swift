443860d16f13e4ecb98a8c7ee7d34da82c669421
swiftcode <swiftcode@users.noreply.github.com>
2025-10-01 02:35:28 -0500

    Networking Definitions + Protocols
    Create basic http method, networking errors and endpoints
    
    Changes to be committed:
    	new file:   Goldspar/Enum/HTTPMethod.swift
    	new file:   Goldspar/Enum/NetworkError.swift
    	new file:   Goldspar/Enum/UserEndpoint.swift
    	new file:   Goldspar/Protocols/Endpoint.swift

---
+++ Goldspar/Enum/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  Goldspar
+//
+//  Created by Michael Campbell on 10/1/25.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
